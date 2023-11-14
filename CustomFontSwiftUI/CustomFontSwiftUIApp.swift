41a0fb8e485687256aa869b440f962424064ff60
netolobo <desilio@gmail.com>
2023-11-14 14:24:46 -0800

    Initial Commit

---
+++ CustomFontSwiftUI/CustomFontSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontSwiftUIApp.swift
+//  CustomFontSwiftUI
+//
+//  Created by Neto Lobo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
