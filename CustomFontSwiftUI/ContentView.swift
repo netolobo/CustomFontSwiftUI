d873b42dfb40c72fbb74c29f505998f4b4effa0c
netolobo <desilio@gmail.com>
2023-11-15 15:24:16 -0800

    Final adjustments

--- CustomFontSwiftUI/ContentView.swift
+++ CustomFontSwiftUI/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 40) {
+            Text("Custom font")
+                .font(.custom("Agbalumo-Regular", size: 35))
+            
+            Text("with")
+                .font(.custom("Lobster-Regular", size: 35))
+            
+            Text("SwiftUI")
+                .font(.custom("Zeyada", size: 35))
         }
-        .padding()
     }
 }
 
